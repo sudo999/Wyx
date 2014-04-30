203ef4624ec7de04514ea6e5c306318c24f1c7c6
Su Norris <su.norris.97@gmail.com>
2014-04-29 20:19:08 -0400

    Added uncompressed source files

---
+++ src/Cmd.java
@@ -0,0 +1,12 @@
+/*	This framework Copyright (C) 2014 Suzanne Norris. 
+ *	For a full copyright statement, see the attached LISENCE.txt
+ */
+public class Cmd {
+	public static void main(String[] args) {
+		
+
+	}
+	
+
+}
+
---
+++ src/Function.java
@@ -0,0 +1,28 @@
+/*	This framework Copyright (C) 2014 Suzanne Norris. 
+ *	For a full copyright statement, see the attached LISENCE.txt
+ */
+
+
+public abstract class Function { //defines a function which "verbs" objects of type object
+	public Function(String name,Class<Object> useOn){
+		verb=name;
+		type=useOn;
+	}
+	public String getVerb(){
+		return verb;
+	}
+	private String verb;
+	private Class<Object> type;
+	protected boolean testExec(Object obj){
+		if(obj.getClass().equals(type)){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	public Object exec(Object obj){
+		return null;
+	}
+
+}
